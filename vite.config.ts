d8aebc204a8983b8af842c9741efa37e6032bb8e
Tanya Yenakieva <yastigma@gmail.com>
2025-03-09 17:41:39 +0200

    fix logo

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,4 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
